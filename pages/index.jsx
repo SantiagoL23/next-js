2448f7a80990f0bb635d19e4646d66ae6636c9f7
Santiago <santiagolorenzut2300@gmail.com>
2021-09-14 21:59:31 -0300

    asd

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Nav } from '../components/navbar/styles'
+import Navbar from "../components/navbar/Navbar";
 import styles from '../styles/Home.module.css'
 
 export default function index() {
   return (
    <>
-<Nav/>
+<Navbar/>
    </>
   )
 }
