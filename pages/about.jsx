4c73c3ea3fad03784cf38ba2ada24a76ce188921
Santiago <santiagolorenzut2300@gmail.com>
2021-09-14 22:04:57 -0300

    f

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Navbar from "../components/navbar/Navbar";
+import Navbar from "../components/navbar/navbar";
 import About from "../components/about_me/about";
 export default function about() {
   return (
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Navbar from "../components/navbar/Navbar";
+import Navbar from "../components/navbar/navbar";
 import styles from '../styles/Home.module.css'
 
 export default function index() {
