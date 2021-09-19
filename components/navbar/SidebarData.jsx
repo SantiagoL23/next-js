9f9aeaafeafd0ed8c4c2ffed7dcb2ffffc743c26
Santiago <santiagolorenzut2300@gmail.com>
2021-09-18 22:56:50 -0300

    interface

---
+++ components/navbar/SidebarData.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const SidebarData = [
+    {
+        title: 'Home',
+        cName: 'navText'
+    },
+    {
+        title: 'Productos',
+        cName: 'navText'
+    },
+    {
+        title: 'Team',
+        cName: 'navText'
+    }
+]
--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -1,31 +1,73 @@
-import React, { useState } from "react";
+import React, { useState, Component } from "react";
 import styled from "styled-components";
-import { Nav, NavLogo, NavMenu, NavButton } from "./styles/index";
+import { FaBars } from "react-icons/fa";
+
 import Link from "next/link";
+import { SidebarData } from "./SidebarData";
 
 export function Navbar() {
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
   return (
     <>
-      <Nav>
-        <NavLogo>
-          <h1>LRZ</h1>
-        </NavLogo>
-        <NavMenu>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
-          <Link href="/contact">
-            <a>Contact</a>
-          </Link>
-        </NavMenu>
-        <NavButton>
-         <Link href="/signin">
-         <button>LOG IN</button>
-         </Link>
-        </NavButton>
-      </Nav>
+      <NavbarItems>
+        <NavbarLogo>
+          <h2>SantiLRZ</h2>
+        </NavbarLogo>
+        <MenuIcon>
+          <FaBars onClick={showSidebar}></FaBars>
+        </MenuIcon>
+
+        <ul className={sidebar ? "nav-menu active" : "nav-menu"}>
+          {SidebarData.map((item, index) => {
+            return (
+              <li key={index} className={item.cName}>
+                     <a>{item.title}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </NavbarItems>
     </>
   );
 }
 
 export default Navbar;
+const NavbarItems = styled.nav`
+  background: black;
+  color:#fff;
+  height: 80px;
+  display: flex;
+ 
+  justify-content: center;
+  align-items: center;
+  ul{
+    padding:0;
+  }
+  
+  @media (max-width: 960px) {
+    position: relative;
+  }
+`;
+const NavbarLogo = styled.div`
+  color: white;
+  justify-self: start;
+  margin-left: 20px;
+  @media(max-width:960px){
+    position:absolute;
+    top:0;
+    left:0;
+
+  }
+`;
+const MenuIcon = styled.div`
+display:none;
+@media(max-width:960px){
+  display:block;
+  position:absolute;
+  top:0;
+  right:0;
+  font-size:1.8rem;
+  transform:translate(-100%, 60%);
+}
+`;
