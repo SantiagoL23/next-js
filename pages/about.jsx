20d590ec43347b2aec4ea803314b27727a51fd90
Santiago <santiagolorenzut2300@gmail.com>
2021-09-18 23:25:38 -0300

    interface

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Navbar from "../components/navbar/Navbar";
+import Navbar from "../components/navbar/navbar";
 
 export default function about() {
   return (
